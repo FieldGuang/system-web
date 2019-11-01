88758e58d875d0b24590c1eb52445658341bb9de
李胜松 <veekergdn.cn@aliyun.com>
2019-11-01 21:02:42 +0800

    'add'

--- src/layout/components/TagsView.vue
+++ src/layout/components/TagsView.vue
@@ -222,23 +222,23 @@ export default {
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 .tags-view-container {
-  height: 34px;
+  height: 45px;
   width: 100%;
-  background: #fff;
-  border-bottom: 1px solid #d8dce5;
+  background: rgb(245,247,249);
+  border-bottom: 1px solid rgb(245,247,249);
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .12), 0 0 3px 0 rgba(0, 0, 0, .04);
   .tags-view-wrapper {
     .tags-view-item {
       display: inline-block;
       position: relative;
       cursor: pointer;
-      height: 26px;
-      line-height: 26px;
-      border: 1px solid #d8dce5;
+      height: 34px;
+      border-radius: 5px;
+      line-height: 34px;
       color: #495060;
       background: #fff;
       padding: 0 8px;
-      font-size: 12px;
+      font-size: 14px;
       margin-left: 5px;
       margin-top: 4px;
       &:first-of-type {
@@ -248,10 +248,10 @@ export default {
         margin-right: 15px;
       }
       &.active {
-        background-color: #42b983;
-        color: #fff;
-        border-color: #42b983;
-        &::before {
+        background-color: #ffffff;
+        color: rgb(45,140,240);
+        border-color: #ffffff;
+        /*&::before {
           content: '';
           background: #fff;
           display: inline-block;
@@ -260,7 +260,7 @@ export default {
           border-radius: 50%;
           position: relative;
           margin-right: 2px;
-        }
+        }*/
       }
     }
   }
@@ -297,11 +297,12 @@ export default {
       height: 16px;
       vertical-align: 2px;
       border-radius: 50%;
+      line-height: 11px;
       text-align: center;
       transition: all .3s cubic-bezier(.645, .045, .355, 1);
       transform-origin: 100% 50%;
       &:before {
-        transform: scale(.6);
+        transform: scale(.8);
         display: inline-block;
         vertical-align: -3px;
       }
